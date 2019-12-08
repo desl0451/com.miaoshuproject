781ef8752a76f923cdfe01e007e7a1f0df891a14
王涛 <desl0451@hotmail.com>
2019-12-08 14:02:26 +0800

    Initial commit

---
+++ src/main/java/com/miaoshaproject/service/impl/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package com.miaoshaproject.service.impl;
+
+
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dao.UserPasswordDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import com.miaoshaproject.dataobject.UserPasswordDO;
+import com.miaoshaproject.service.UserService;
+import com.miaoshaproject.service.model.UserModel;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+    @Autowired
+    private UserPasswordDOMapper userPasswordDOMapper;
+
+    @Override
+    public UserModel getUserById(Integer id) {
+        //调用UserDoMapper获取到对应的用户dataobject
+        UserDO userDo = userDOMapper.selectByPrimaryKey(id);
+        if (userDo == null) {
+            return null;
+        }
+        //通过用户id获取对应的用户加密密码信息
+        UserPasswordDO userPasswordDO = userPasswordDOMapper.selectByUserId(id);
+        return convertFromDataObject(userDo, userPasswordDO);
+    }
+
+    private UserModel convertFromDataObject(UserDO userDO, UserPasswordDO userPasswordDO) {
+        if (userDO == null) {
+            return null;
+        }
+        UserModel userModel = new UserModel();
+        BeanUtils.copyProperties(userDO, userModel);
+        if (userPasswordDO != null) {
+            userModel.setEncrpPassword(userPasswordDO.getEncrptPassword());
+        }
+        return userModel;
+    }
+}
---
+++ src/main/java/com/miaoshaproject/service/model/UserModel.java
@@ -0,0 +1,77 @@
+package com.miaoshaproject.service.model;
+
+public class UserModel {
+    private Integer id;
+    private String name;
+    private Integer gender;
+    private Integer age;
+    private String telphone;
+    private String registerModel;
+    private String thirdPartyId;
+
+    private String encrpPassword;
+
+    public String getEncrpPassword() {
+        return encrpPassword;
+    }
+
+    public void setEncrpPassword(String encrpPassword) {
+        this.encrpPassword = encrpPassword;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+
+    public String getRegisterModel() {
+        return registerModel;
+    }
+
+    public void setRegisterModel(String registerModel) {
+        this.registerModel = registerModel;
+    }
+
+    public String getThirdPartyId() {
+        return thirdPartyId;
+    }
+
+    public void setThirdPartyId(String thirdPartyId) {
+        this.thirdPartyId = thirdPartyId;
+    }
+}
